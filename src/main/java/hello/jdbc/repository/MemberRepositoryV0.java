28086b09e5448015aed02a89dd82bdce95c8fd2e
hongjunpyo11 <jpyo98@naver.com>
2023-01-02 17:52:38 +0900

    JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -17,7 +18,6 @@ public class MemberRepositoryV0 {
 
         Connection con = null;
         PreparedStatement pstmt = null;
-
 
         try {
             con = getConnection();
@@ -33,6 +33,36 @@ public class MemberRepositoryV0 {
             close(con, pstmt, null);
         }
 
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);;
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
     }
 
     private void close(Connection con, Statement stmt, ResultSet rs) {
@@ -67,3 +97,17 @@ public class MemberRepositoryV0 {
         return DBConnectionUtil.getConnection();
     }
 }
+
+/**
+ * ResultSet
+ *   * ResultSet 은 다음과 같이 생긴 데이터 구조이다. 보통 select 쿼리의 결과가 순서대로 들어간다.
+ *     * 예를 들어서 select member_id, money 라고 지정하면 member_id , money 라는 이름으로 데이터가 저장된다.
+ *   * 참고로 select * 을 사용하면 테이블의 모든 컬럼을 다 지정한다.
+ *   * ResultSet 내부에 있는 커서( cursor )를 이동해서 다음 데이터를 조회할 수 있다.
+ *   * rs.next() : 이것을 호출하면 커서가 다음으로 이동한다. 참고로 최초의 커서는 데이터를 가리키고 있지 않기 때문에 rs.next() 를 최초 한번은 호출해야 데이터를 조회할 수 있다.
+ *     * rs.next() 의 결과가 true 면 커서의 이동 결과 데이터가 있다는 뜻이다.
+ *     * rs.next() 의 결과가 false 면 더이상 커서가 가리키는 데이터가 없다는 뜻이다.
+ *   * rs.getString("member_id") : 현재 커서가 가리키고 있는 위치의 member_id 데이터를 String 타입으로 반환한다.
+ *   * rs.getInt("money") : 현재 커서가 가리키고 있는 위치의 money 데이터를 int 타입으로 반환한다.
+ *
+ */
